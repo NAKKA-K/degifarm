ffe33ddd17f5e867caf7687571db63caf98d608f
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2017-08-03 03:41:29 +0900

    正規表現で条件を指定

--- vagrant_add.sh
+++ vagrant_add.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-packageName='samurai'
+packageName='package.box'
 #vagrant box add dw2018_server "${packageName}"
 
 #指定のパッケージが追加されているか判定
 boxList="`vagrant box list | grep "${packageName}"`"
-echo $boxList
+echo "${boxList}"
 
-if [ boxList == ".*${packageName}.*" ]; then
+if [[ "${boxList}" =~ (.*)${packageName}(.*) ]]; then
   echo OK! Added package!
 else
   echo NO! Not added package!
