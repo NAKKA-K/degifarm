f97192c142ae3785ff0afb35b34b6fcbf149f4e3
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2017-08-03 02:39:06 +0900

    パッケージ追加の成否を出力するようにした

--- vagrant_add.sh
+++ vagrant_add.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-vagrant box add dw2018_server package.box
+packageName = 'package.box'
+vagrant box add dw2018_server $packageName
 
+if [ vagrant box list | grep "${packageName}" != "" ]; then
+  echo OK! Added package!
+else
+  echo NO! Not added package!
+fi
