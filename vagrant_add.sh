cd606a454caddb75f917856b95fe5de5121ee8a0
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2017-08-03 03:46:34 +0900

    本番用にコメントを外した

--- vagrant_add.sh
+++ vagrant_add.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 packageName='package.box'
-#vagrant box add dw2018_server "${packageName}"
+vagrant box add dw2018_server "${packageName}"
 
 #指定のパッケージが追加されているか判定
 boxList="`vagrant box list | grep "${packageName}"`"
-echo "${boxList}"
 
 if [[ "${boxList}" =~ (.*)${packageName}(.*) ]]; then
   echo OK! Added package!
