5c42fb6db515becc5d5d53911282f31ecb2a0ad8
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2017-08-03 03:28:45 +0900

    listの抽出方法を変更

--- vagrant_add.sh
+++ vagrant_add.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-packageName='package.box'
+packageName='samurai'
 #vagrant box add dw2018_server "${packageName}"
 
 #指定のパッケージが追加されているか判定
-boxList=`vagrant box list | grep "${packageName}"`
+boxList="`vagrant box list | grep "${packageName}"`"
 echo $boxList
 
-if [ boxList != "" ]; then
+if [ boxList == ".*${packageName}.*" ]; then
   echo OK! Added package!
 else
   echo NO! Not added package!
