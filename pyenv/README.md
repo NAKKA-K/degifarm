7dcad6d0d35833216f08d22db79e274f7d76f513
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2017-08-06 19:26:49 +0900

    Add pyenv installer & Change directory.
    どれがどのファイルか分かりずらくなるため、それぞれディレクトリを作って分けるようにした。

---
+++ pyenv/README.md
@@ -0,0 +1,8 @@
+# pyenv_installer
+pyenvをインストールするスクリプト
+
+# Install手順
+
+    git clone https://github.com/NAKKA-K/dw2018_server
+    chmod +x pyenv_installer.sh
+    ./pyenv_installer
---
+++ pyenv/pyenv_installer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+
+#Write pyenv setting on ~/.bash_profile
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+echo 'export PATH="${PYENV_ROOT}/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+
+#~/.bash_profileの変更を更新
+source ~/.bash_profile
