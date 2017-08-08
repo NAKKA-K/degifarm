f0d2932bedc9e867bd059e23e3d9a1d7b6cd73fd
YuiOgoshi <kd1243444@st.kobedenshi.ac.jp>
2017-08-09 01:55:17 +0900

    なんかコード見にくかったから少し書き直した

--- install.bat
+++ install.bat
@@ -7,16 +7,16 @@ set file="false"
 if exist %virtual% if exist %git% if exist %vagrant% set file="true"
 
 if %file%=="true" (
-			echo OK、インストールを開始します
-			rem VirtualBoxをインストール
-			call %virtual%
+	echo OK、インストールを開始します
+	rem VirtualBoxをインストール
+	call %virtual%
 
-			rem gitをインストール
-			call %git%
+	rem gitをインストール
+	call %git%
 
-			rem Vagrantをインストール
-			msiexec/i %vagrant%
+	rem Vagrantをインストール
+	msiexec/i %vagrant%
 ) else (
-			echo いずれかのファイルが見つかりませんでした。
+	echo いずれかのファイルが見つかりませんでした。
 )
 pause
