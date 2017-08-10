7cc5419da0dfafe364159ee3b84708a24a3ad5d6
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2017-08-10 14:04:11 +0900

    fixed vagrant_add.sh

--- vagrant/vagrant_add.sh
+++ vagrant/vagrant_add.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 packageName='package.box'
-vagrant box add dw2018_server "${packageName}"
+vagrantBoxName='dw2018_server'
+vagrant box add "${vagrantBoxName}" "${packageName}"
 
 #指定のパッケージが追加されているか判定
-boxList="`vagrant box list | grep "${packageName}"`"
+boxList="`vagrant box list | grep "${vagrantBoxName}"`"
 
-if [[ "${boxList}" =~ (.*)${packageName}(.*) ]]; then
-  echo OK! Added package!
+if [[ "${boxList}" =~ (.*)${vagrantBoxName}(.*) ]]; then
+  echo OK! Added box!
 else
-  echo NO! Not added package!
+  echo NO! Not added box!
 fi
