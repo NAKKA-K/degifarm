fa233db1b7aaa28bab0e5c96fa8c7d57620ebf67
oya0831 <kd1243444@st.kobedenshi.ac.jp>
2017-08-12 20:52:08 +0000

    あちゃーちょっと抜けがあったしプログラム実行する前にコメント出すようになってるとこ見つけたー

--- postgresql/psql_installer.sh
+++ postgresql/psql_installer.sh
@@ -1,18 +1,18 @@
 #postgresqlをインストール
 sudo apt-get update
 sudo apt-get -y install postgresql
-
 echo 'postgresqlをインストールしました'
 
 #58行目に他ホストからもアクセスできるように追加
 sudo sed -i -e "58a listen_address = '*'" /etc/postgresql/9.4/main/postgresql.conf
 
-#posgreを再起動
+#posgreを起動
 sudo /etc/init.d/postgresql start
+echo 'postgresqlを起動しました'
 
 #postgresql管理者アカウントのパスワードを新しく設定(postgresに統一)
-echo 'パスワードを設定してください。postgresに統一します。'
 sudo passwd postgres
+echo 'パスワードを設定してください。postgresに統一します。'
 
 #postgresqlにログイン
 su - postgres
