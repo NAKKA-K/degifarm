d42bb8351aa7926eb9012c5a576d01a346b015d7
oya0831 <kd1243444@st.kobedenshi.ac.jp>
2017-08-16 20:09:25 +0000

    一応改行コードの書き直しと、psql_setdb.shをpostgresユーザのホームディレクトリにあらかじめコピーさせておいて実行を楽に

--- postgresql/psql_installer.sh
+++ postgresql/psql_installer.sh
@@ -15,13 +15,17 @@ echo -e 'postgresqlを起動しました\n'
 #postgresql管理者アカウントのパスワードを新しく設定(postgresに統一)
 echo -e 'パスワードを設定してください。postgresに統一します。\n'
 sudo passwd postgres
-echo -e 'postgreユーザのパスワードを設定しました\n'
+echo -e 'postgresユーザのパスワードを設定しました\n'
 
 #develop17ユーザを作成
 echo -e '開発用に使用するdevelop17ユーザを作成します。\nパスワードはdevelop17(2回入力)に統一してください。\n入力し終わった後は、5回エンターキーを押してもらった後にIs the information correct?と聞かれるので、Yを入力して下さい。\n'
 sudo adduser develop17
 echo -e 'develop17ユーザを作成しました\n'
 
+#postgresユーザのホームディレクトリにpsql_setdb.shを実行できるようにコピーしておく
+sudo cp ~/dw2018_server/postgresql/psql_setdb.sh /var/lib/postgresql/psql_setdb.sh 
+echo -e 'psql_setdbをpostgresユーザのホームディレクトリに移動しました\n'
+
 #postgresqlにログイン
-echo 'postgresユーザに切り替えます。パスワードはpostgresです。\n'
+echo -e 'postgresユーザに切り替えます。パスワードはpostgresです。\n'
 su - postgres
