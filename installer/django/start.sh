7656160ee432ca01515f98cb8b58880f498086f1
oya0831 <kd1243444@st.kobedenshi.ac.jp>
2017-09-06 04:59:43 +0000

    書き直し

--- installer/django/start.sh
+++ installer/django/start.sh
@@ -2,13 +2,9 @@
 django-admin startproject projecttest
 
 #設定ファイルに書き込み
-echo 'DATABASES = {' >> ~/projecttest/projecttest/setting.py
-echo '   'default': {' >> ~/projecttest/projecttest/setting.py
-echo '        'ENGINE': 'django.db.backends.postgresql_psycopg2',' >> ~/projecttest/projecttest/setting.py
-echo '        'NAME': 'dw2018db',' >> ~/projecttest/projecttest/setting.py
-echo '        'USER': 'develop17',' >> ~/projecttest/projecttest/setting.py
-echo '        'PASSWORD' : 'develop17',' >> ~/projecttest/projecttest/setting.py
-echo '        'HOST' : '127.0.0.1',' >> ~/projecttest/projecttest/setting.py
-echo '        'PORT' : 5432,' >> ~/projecttest/projecttest/setting.py
-echo '    }' >> ~/projecttest/projecttest/setting.py
-echo '}' >> ~/projecttest/projecttest/setting.py
+sed -i -e "s/'django.db.backends.sqlite3'/'django.db.backends.postgresql_psycopg2'/g" ~/projecttest/projecttest/settings.py
+sed -i -e "s/os.path.join(BASE_DIR, 'db.sqlite3')/'dw2018db'/g" ~/projecttest/projecttest/settings.py 
+sed -i -e "79a\        \'USER': 'develop17'," ~/projecttest/projecttest/settings.py
+sed -i -e "80a\        \'PASSWORD' : 'develop17'," ~/projecttest/projecttest/settings.py
+sed -i -e "81a\        \'HOST' : '127.0.0.1'," ~/projecttest/projecttest/settings.py
+sed -i -e "82a\        \'PORT' : 5432," ~/projecttest/projecttest/settings.py
