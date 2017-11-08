37fb257e0852a639167a497b88b6e0d372c6e1dd
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2017-11-08 05:17:05 +0000

    gitのインストールを別で

--- installer/pyenv/pyenv_installer.sh
+++ installer/pyenv/pyenv_installer.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-#install git
-sudo apt-get -y install git
-
+sudo apt-get update
 
 #pyenv download
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
@@ -12,4 +10,8 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
 echo 'export PATH="${PYENV_ROOT}/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
 
+
+
 #~/.bash_profileの変更を更新($ source ~/.bash_profile)
+echo '$ source ~/.bash_profile を実行してください。'
+echo 'その後、$ ./pyenv_setting.sh を実行してください'
--- installer/pyenv/pyenv_setting.sh
+++ installer/pyenv/pyenv_setting.sh
@@ -6,3 +6,4 @@ sudo apt-get -y install libssl-dev libbz2-dev libreadline-dev libsqlite3-dev
 
 #install python-3.5.0
 pyenv install 3.5.0
+
