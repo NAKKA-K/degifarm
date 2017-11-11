24e08c0d926dcf34c888288d0725b05cb136246a
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2017-11-11 13:33:23 +0000

    djangoプロジェクトの説明追加

---
+++ degifarm/README.md
@@ -0,0 +1,8 @@
+# パッケージのインストール
+
+    $ pip install -r requirements.txt
+
+# テストサーバーの起動
+
+    ./runserver.sh
+
--- installer/django/start.sh
+++ installer/django/start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+pip install -r requirements.txt
+
 project=projecttest
 #djangoのプロジェクト作成
 django-admin startproject ${project}
