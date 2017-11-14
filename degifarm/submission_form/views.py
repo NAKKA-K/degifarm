26b301ea2892ee8a3c2e88ab4aee7890f13340a3
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2017-11-14 14:40:11 +0000

    ファイルアップロードされた時に、成功画面にファイルデータを共に飛ばすように記述

--- degifarm/submission_form/views.py
+++ degifarm/submission_form/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+from .forms import UploadFileForm
 
 # Create your views here.
 
 def index(request):
   return render(request, 'submission_form/index.html', {})
+
+def upload_file(request):
+  if request.method == 'POST':
+    form = UploadFileForm(request.POST, request.FILES)
+    if form.is_valid():
+      file_data = read_file(request.FILES['file'])
+      return render(request, 'submission_form/sucess.html', {
+        'file_data': file_data,
+      })
+  else:
+    form = UploadFileForm()
+  return render(request, 'submission_form/upload_file.html', {
+    'form': form,
+  })
+
+def read_file(file_source):
+  data = file_source.name + ' : '
+  for chunk in file_source.chunks():
+    data = data + str(chunk)
+
+  return data
