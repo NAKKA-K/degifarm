4f1526f2a96acd7e0556f25ba6f04164f79b2c05
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2017-11-15 02:43:42 +0000

    fix ファイルを正常に表示

--- degifarm/submission_form/views.py
+++ degifarm/submission_form/views.py
@@ -6,23 +6,34 @@ from .forms import UploadFileForm
 def index(request):
   return render(request, 'submission_form/index.html', {})
 
+
 def upload_file(request):
   if request.method == 'POST':
     form = UploadFileForm(request.POST, request.FILES)
     if form.is_valid():
-      file_data = read_file(request.FILES['file'])
-      return render(request, 'submission_form/sucess.html', {
+      file_data = read_file(request.FILES['file']) # ファイル取得(メモリ展開状態)したものを、文字列に変換
+
+      return render(request, 'submission_form/sucess.html', { # アップロード完了ページに遷移
         'file_data': file_data,
       })
+
   else:
     form = UploadFileForm()
+
   return render(request, 'submission_form/upload_file.html', {
     'form': form,
   })
 
+
+# メモリ展開されたバイトデータを文字列に変換する
 def read_file(file_source):
-  data = file_source.name + ' : '
+  data = file_source.name + ' : \n'
   for chunk in file_source.chunks():
     data = data + chunk.decode('utf-8')
 
   return data
+
+
+
+
+
