ac45fda5fa0c0907845b1846cb7ef7584d0b8037
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2017-11-16 00:47:35 +0000

    フォームクラスを呼び出すように変更

--- degifarm/submission_form/urls.py
+++ degifarm/submission_form/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
   url(r'^$', views.index, name = 'index'),
-  url(r'^upload$', views.upload_file, name = 'upload')
+  url(r'^upload$', views.UploadFilesView.as_view(), name = 'upload')
 ]
