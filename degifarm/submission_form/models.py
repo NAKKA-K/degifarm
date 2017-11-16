549e9e4cf98ebd47ac6098143ba13dbf27efc031
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2017-11-16 05:27:38 +0000

    簡易データベース作成

--- degifarm/submission_form/models.py
+++ degifarm/submission_form/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+# 団体テーブル
+class Qrganization(models.Model):
+  uuid = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+  name = models.CharField(max_length = 64)
+
+# 性別マスタ
+class Sex(models.Model):
+  name = CharField(max_length = 4)
+
+# 学科テーブル
+class Group(models.Model):
+  organization_id = models.ForeignKey(Organization, primary_key = True)
+  id = models.IntegerField(primary_key = True)
+  name = models.CharField(max_length = 64)
+
+# 分類テーブル
+class Classification(models.Model):
+  organization_id = models.ForeignKey(Qrganization, primary_key = True)
+  id = models.IntegerField(primary_key = True)
+  name = models.CharField(max_length = 64)
+
+# 学生テーブル
+class Student(models.Model):
+  organization_id = models.ForeignKey(Organization, primary_key = True)
+  id = models.CharField(primary_key = True, max_length = 32)
+  group_id = models.ForeignKey(Group)
+  sex_id = models.ForeignKey(Sex)
+  name = models.CharField(null = False, max_length = 64)
+  # TODO: 学年、クラス、番号をどうするか？
+  email = models.EmailField(null = False)
+
+# 提出物テーブル
+class Submission(models.Model):
+  organization_id = models.ForeignKey(Organization, primary_key = True)
+  student_id = models.ForeignKey(Student, primary_key = True)
+  uuid = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+  classification_id = models.ForeignKey(Classification)
+  name = models.CharField(max_length = 64)
+  published_date = models.DataTimeField(null = False, default = timezpne.now, editable = False)
+  # TODO: 権限をどうするか？
+  path = models.CharField(max_length = 128)
+
+
+
+
+
+
