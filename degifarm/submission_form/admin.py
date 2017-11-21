732c0180f8f1016a09f6c302519ea9e1b9b56cac
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2017-11-21 01:31:52 +0000

    adminサイトにテーブルを表示

--- degifarm/submission_form/admin.py
+++ degifarm/submission_form/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Organization, Sex, Group, Classification, Student, Submission, Teacher, Distribution
+
 # Register your models here.
+
+admin.site.register(Organization)
+admin.site.register(Sex)
+admin.site.register(Group)
+admin.site.register(Classification)
+admin.site.register(Student)
+admin.site.register(Submission)
+admin.site.register(Teacher)
+admin.site.register(Distribution)
