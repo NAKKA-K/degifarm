e0a31ab48e179d771a31f45fbe3c51205c837a2e
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-01-16 06:43:33 +0000

    Add サイトにアップロードされた複数のファイル(UploadedFileのリスト)を受け取って、個別にサーバーに格納するクラスを作成

--- submission_form/views/FileUploader.py
+++ submission_form/views/FileUploader.py
@@ -4,34 +4,57 @@ from django.conf import settings
 # app module
 
 # lib
+import mimetypes
+import os
 
 
-class FileUploader(Object):
-
-  def __init__(self, files):
+class FileUploader(object):
+  """
+  UploadFileオブジェクトのリストを受け取って、色々処理してくれるクラス。
+  """
+  def __init__(self, files, user = '__TMP__'):
     self.files = files
+    self.files_path_list = []
+    self.user = user
+    self.files_dir = '{}{}'.format(settings.MEDIA_ROOT, user) # TODO:後に課題ごとなどにディレクトリをまとめる可能性あり
 
-  def handle_uploaded_files():
+  def handle_uploaded_files(self):
+    """
+    FileUploaderに渡された複数ファイルをすべて保存する。
+    保存ディレクトリが存在しない場合は、ディレクトリを作成する。
+    """
+    self.make_dir()
     for f in self.files:
-      file = File(f)
+      file = self.UploadFileHandler(f, self.files_dir)
       file.handle_uploaded_file()
+      self.files_path_list.append(file.get_full_path())
+
+  def make_dir(self):
+    if not os.path.exists(self.files_dir):
+      os.makedirs(self.files_dir)
 
   
-  class File(Object):
+  class UploadFileHandler(object):
+    """
+    UploadFileクラスを受け取って、ファイルとして保存するクラス。
+    """
 
-    def __init__(self, file):
+    def __init__(self, file, save_dir):
       self.file = file
-      self.name = self.file.name
-      self.path = '{}'.format(settings.MEDIA_ROOT) # TODO:後に課題ごとなどにディレクトリをまとめる可能性あり
+      self.save_dir = save_dir
 
     def handle_uploaded_file(self):
-      make_dir()
-
-      with open('{}{}'.format(self.path, self.name), 'wb+') as dest:
+      """
+      インスタンス生成時に渡されたUploadFileオブジェクトを、
+      外部クラスのFileUploaderのfiles_dirパス以下に保存する。
+      """
+      with open(self.get_full_path(), 'wb+') as dest:
         for chunk in self.file.chunks():
           dest.write(chunk)
 
-    def make_dir(self):
-      if not os.path.exists(self.path):
-        os.makedirs(self.path)
+    def get_full_path(self):
+      return '{}{}'.format(self.save_dir, self.file.name)
+      
+    def get_mime_types(self):
+      return mimetypes.guess_type(self.get_full_path())
 
