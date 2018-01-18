c77c2297368e9f0e74b2d1915f8264f538628006
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-01-18 03:06:48 +0000

    Use ローカル環境ではSQLiteを使うように変更

--- degifarm/settings.py
+++ degifarm/settings.py
@@ -161,6 +161,13 @@ DEBUG = False
 
 try:
   from .local_settings import *
+
+  DATABASES = {
+    'default': {
+      'ENGINE': 'django.db.backends.sqlite3',
+      'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+  }
 except ImportError:
   pass
 
