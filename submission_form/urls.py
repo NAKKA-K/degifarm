5f56a330ce32c6e22b945bbc5b85026ca04fef4f
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-01-21 09:49:52 +0000

    Fix nameが先頭大文字になっていたので修正

--- submission_form/urls.py
+++ submission_form/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
   url(r'^task/$', views.TaskHomeView.as_view(), name = 'task_index'),
   url(r'^upload/form/$', views.UploadFilesView.as_view(), name = 'upload_form'),
-  url(r'^upload/$', views.UploadList.as_view(), name = 'Upload_index')
+  url(r'^upload/$', views.UploadList.as_view(), name = 'upload_index')
 ]
