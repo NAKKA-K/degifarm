1a0827d523025c6628125c72d8343b0e6ab05e27
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-01-21 14:13:40 +0000

    Add 提出物の分類をモデルから表示させるように変更

--- submission_form/views/upload.py
+++ submission_form/views/upload.py
@@ -1,8 +1,14 @@
 from django.views.generic.list import ListView
 
-from submission_form.models import Submission
+from submission_form.models import Submission, Classification
+
 
 class UploadList(ListView):
   model = Submission
   template_name = 'submission_form/upload_list.html'
   context_object_name = 'upload_list'
+
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context['classification'] = Classification.objects.all()
+    return context
