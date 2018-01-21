da099f0d220fab7c98cf138a8e75e8f877332ba2
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-01-21 09:08:27 +0000

    Add submissionテーブルをListViewで表示させる

---
+++ submission_form/views/upload.py
@@ -0,0 +1,8 @@
+from django.views.generic.list import ListView
+
+from submission_form.models import Submission
+
+class UploadList(ListView):
+  model = Submission
+  template_name = 'submission_form'
+  context_object_name = 'upload_list'
