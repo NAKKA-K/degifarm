84d722757e8de438ad3756bea59265c9917f7133
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-01-21 13:39:08 +0000

    Use 分かりやすいように、名称等を変更

--- submission_form/urls.py
+++ submission_form/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-  url(r'^task/$', views.TaskHomeView.as_view(), name = 'task_index'),
+  url(r'^$', views.TaskHomeView.as_view(), name = 'index'),
   url(r'^upload/form/$', views.UploadFilesView.as_view(), name = 'upload_form'),
   url(r'^upload/$', views.UploadList.as_view(), name = 'upload_index')
 ]
