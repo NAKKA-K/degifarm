d8d88b3ec80150839c2d9345062f591bf4642963
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-01-22 10:07:23 +0000

    Add アップロードするファイルの分野を指定できるように、フォームを拡張

--- submission_form/forms.py
+++ submission_form/forms.py
@@ -1,4 +1,8 @@
 from django import forms
+from submission_form.models import Classification
+from submission_form.views.StudentOrTeacherGetter import StudentOrTeacherGetter
 
 class UploadFilesForm(forms.Form):
+    classification = forms.ModelChoiceField(Classification.objects, label = '分野')
     files = forms.FileField(widget = forms.ClearableFileInput(attrs = {'multiple': True}))
+
