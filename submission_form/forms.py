7889a2f301d3d2cbca6062a3ab5875b24c9939f4
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-01-23 00:42:48 +0000

    Add Org毎にquerysetにフィルターを書ける処理を追加

--- submission_form/forms.py
+++ submission_form/forms.py
@@ -6,3 +6,7 @@ class UploadFilesForm(forms.Form):
     classification = forms.ModelChoiceField(Classification.objects, label = '分野')
     files = forms.FileField(widget = forms.ClearableFileInput(attrs = {'multiple': True}))
 
+    def __init__(self, org_id = None, *args, **kwargs):
+      super().__init__(*args, **kwargs)
+      self.fields['classification'].queryset = Classification.objects.filter(organization_id = org_id)
+
