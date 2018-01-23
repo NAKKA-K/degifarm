d279a62ace07ed13dbe37341b94d15fb523eabff
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-01-23 15:19:22 +0000

    Update フォームに表示される情報を分かりやすく変更

--- submission_form/models/task.py
+++ submission_form/models/task.py
@@ -7,8 +7,8 @@ from submission_form.models import Organization, Classification
 class Task(models.Model):
   organization_id = models.ForeignKey(Organization)
   user_id = models.ForeignKey(settings.AUTH_USER_MODEL)
-  classification_id = models.ForeignKey(Classification)
-  name = models.CharField(max_length = 64)
-  text = models.TextField()
-  deadline = models.DateTimeField()
+  classification_id = models.ForeignKey(Classification, verbose_name = '科目')
+  name = models.CharField(max_length = 64, verbose_name = '提出ファイル名')
+  text = models.TextField(verbose_name = '説明')
+  deadline = models.DateTimeField(verbose_name = '提出期限', help_text = 'xxxx-xx-xx の形式で入力してください')
   published_date = models.DateTimeField(default = timezone.now, editable = False)
