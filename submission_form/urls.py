377c6220d6717ac47e3cf6df1ef88b17db48801a
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-01-23 15:21:37 +0000

    Add 課題の追加ページのURLを追加

--- submission_form/urls.py
+++ submission_form/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
   url(r'^$', views.TaskHomeView.as_view(), name = 'index'),
+  url(r'^upload/$', views.UploadList.as_view(), name = 'upload_index'),
   url(r'^upload/form/$', views.UploadFilesView.as_view(), name = 'upload_form'),
-  url(r'^upload/$', views.UploadList.as_view(), name = 'upload_index')
+
+  # 先生のみアクセス可
+  url(r'^task/create/$', views.TaskCreateView.as_view(), name = 'task_create'),
 ]
