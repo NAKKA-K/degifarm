114f07453612a334dbec051816af02f7a3df0d9b
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-01-24 11:58:23 +0000

    Add モデルにパスを登録することで、CreateViewやDeleteViewからのアクセスを自動的にするように実装

--- submission_form/models/task.py
+++ submission_form/models/task.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
+from django.urls import reverse
 
 from submission_form.models import Organization, Classification
 
@@ -12,3 +13,6 @@ class Task(models.Model):
   text = models.TextField(verbose_name = '説明')
   deadline = models.DateTimeField(verbose_name = '提出期限', help_text = 'xxxx-xx-xx の形式で入力してください')
   published_date = models.DateTimeField(default = timezone.now, editable = False)
+
+  def get_absolute_url(self, *args, **kwargs):
+    return reverse('submission_form:task_detail', kwargs = {'pk':self.pk})
