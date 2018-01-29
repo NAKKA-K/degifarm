382e79c757746e8eb4f90ba59240c1b92ccfe371
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-01-29 02:18:48 +0000

    Add 新しいページへのアクセステストを追加

--- submission_form/tests/SiteGetTest.py
+++ submission_form/tests/SiteGetTest.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 
+from submission_form.models import User
+
 class SiteGetTest(TestCase):
   def setUp(self):
     User.objects.create_user(email = 'test@test.com', password = 'testpass1')
@@ -9,6 +11,7 @@ class SiteGetTest(TestCase):
 
     # non logined
     self.assertEqual(client.get('/').status_code, 200)
+    self.assertEqual(client.get('/accounts/').status_code, 200)
     self.assertEqual(client.get('/accounts/login/').status_code, 200)
     self.assertEqual(client.get('/accounts/logout/').status_code, 302)
     self.assertEqual(client.get('/submission_form/').status_code, 302)
@@ -21,8 +24,10 @@ class SiteGetTest(TestCase):
     client.login(email = 'test@test.com', password = 'testpass1')
 
     self.assertEqual(client.get('/').status_code, 200)
+    self.assertEqual(client.get('/accounts/').status_code, 200)
     self.assertEqual(client.get('/accounts/login/').status_code, 200)
-    self.assertEqual(client.get('/accounts/logout/').status_code, 302)
     self.assertEqual(client.get('/submission_form/').status_code, 200)
     self.assertEqual(client.get('/submission_form/upload/').status_code, 200)
     self.assertEqual(client.get('/submission_form/upload/form/').status_code, 200)
+
+    self.assertEqual(client.get('/accounts/logout/').status_code, 302)
