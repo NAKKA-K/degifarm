6d0dd8481f94ed0e3c640d1533bc49ec6647c391
oya0831 <kd1243444@st.kobedenshi.ac.jp>
2018-01-29 09:02:50 +0000

    fixed

--- submission_form/forms.py
+++ submission_form/forms.py
@@ -1,11 +1,8 @@
 from django import forms
-<<<<<<< HEAD
 from submission_form.models import Classification,Distribution
-=======
 from django.contrib.auth.forms import UserCreationForm
 
 from submission_form.models import Classification, Organization, Teacher, User
->>>>>>> 5b2b724191adde0879dab87f6153853c16057d84
 from submission_form.views.StudentOrTeacherGetter import StudentOrTeacherGetter
 
 class UploadFilesForm(forms.Form):
@@ -16,7 +13,6 @@ class UploadFilesForm(forms.Form):
       super().__init__(*args, **kwargs)
       self.fields['classification'].queryset = Classification.objects.filter(organization_id = org_id)
 
-<<<<<<< HEAD
 
 class FileForm(forms.ModelForm):
     """Fileモデルのフォーム."""
@@ -53,7 +49,7 @@ class CategoryForm(forms.ModelForm):
                 'class': 'form-control',
             }),
         }
-=======
+
 class OrganizationForm(forms.ModelForm):
   class Meta:
     model = Organization
@@ -68,6 +64,4 @@ class TeacherForm(forms.ModelForm):
   class Meta:
     model = Teacher
     fields = ['sex_id']
-
->>>>>>> 5b2b724191adde0879dab87f6153853c16057d84
 
--- submission_form/views/__init__.py
+++ submission_form/views/__init__.py
@@ -2,9 +2,6 @@ from submission_form.views.home import *
 from submission_form.views.task import *
 from submission_form.views.upload import *
 from submission_form.views.StudentOrTeacherGetter import *
-<<<<<<< HEAD
 from submission_form.views.DistributionController import *
-=======
 
 from submission_form.views.service_start import *
->>>>>>> 5b2b724191adde0879dab87f6153853c16057d84
