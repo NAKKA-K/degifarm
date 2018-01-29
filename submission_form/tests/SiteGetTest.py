4e0fa6ba97c7b9f98c71af76f210e5b75e79c216
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-01-29 02:38:39 +0000

    Add ユーザー登録ページへのアクセス権のテスト

--- submission_form/tests/SiteGetTest.py
+++ submission_form/tests/SiteGetTest.py
@@ -4,7 +4,16 @@ from submission_form.models import User
 
 class SiteGetTest(TestCase):
   def setUp(self):
-    User.objects.create_user(email = 'test@test.com', password = 'testpass1')
+    org = Organization.objects.create(name = 'test org')
+    group = Group.objects.create(organization_id = org, name = 'test group')
+    sex = Sex.objects.create(name = '男性')
+
+    student_user = User.objects.create_user(email = 'test@test.com', password = 'testpass1')
+    Student.objects.create(user = student_user, organization_id = org, group_id = group, sex_id = sex)
+
+    teacher_user = User.objects.create_user(email = 'test@test.teacher', password = 'testpass1')
+    Teacher.objects.create(user = teacher_user, organization_id = org, group_id = group, sex_id = sex)
+    
 
   def test_get_all_page(self):
     client = self.client
@@ -17,7 +26,7 @@ class SiteGetTest(TestCase):
     self.assertEqual(client.get('/submission_form/').status_code, 302)
     self.assertEqual(client.get('/submission_form/upload/').status_code, 302)
     self.assertEqual(client.get('/submission_form/upload/form/').status_code, 302)
-
+    self.assertEqual(client.get('/accounts/create/').status_code, 302)
 
 
     # login
@@ -29,5 +38,22 @@ class SiteGetTest(TestCase):
     self.assertEqual(client.get('/submission_form/').status_code, 200)
     self.assertEqual(client.get('/submission_form/upload/').status_code, 200)
     self.assertEqual(client.get('/submission_form/upload/form/').status_code, 200)
+    self.assertEqual(client.get('/accounts/create/').status_code, 302)
 
     self.assertEqual(client.get('/accounts/logout/').status_code, 302)
+
+
+  def test_account_create_page(self):
+    client = self.client
+
+    self.assertEqual(client.get('/accounts/create/').status_code, 302)
+
+    # 生徒は入れない
+    client.login(email = 'test@test.com', password = 'testpass1')
+    self.assertEqual(client.get('/accounts/create/').status_code, 302)
+
+    # 先生は入れる
+    client.login(email = 'test@test.teacher', password = 'testpass1')
+    self.assertEqual(client.get('/accounts/create/').status_code, 200)
+
+
