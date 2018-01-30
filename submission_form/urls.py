88c27ddbde182499b85a69cd79f79e282881c164
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-01-30 09:52:48 +0000

    Add ダウンロード用のURLを作成

--- submission_form/urls.py
+++ submission_form/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
   url(r'^$', views.TaskHomeView.as_view(), name = 'index'),
   url(r'^upload/$', views.UploadList.as_view(), name = 'upload_index'),
   url(r'^upload/form/$', views.UploadFilesView.as_view(), name = 'upload_form'),
+  url(r'^submission/download/(?P<pk>.*)/$',views.DownloadSubView.as_view(), name='sub_download'),
+
   # 先生のみアクセス可
   url(r'^task/create/$', views.TaskCreateView.as_view(), name = 'task_create'),
   url(r'^task/(?P<pk>[0-9]+)/$', views.TaskDetailView.as_view(), name = 'task_detail'),
@@ -18,6 +20,7 @@ urlpatterns = [
   url(r'^distribution/create/$', views.FileCreateView.as_view(), name='dist_create'),
   url(r'^distribution/update/(?P<pk>.*)/$',views.FileUpdateView.as_view(), name='dist_update'),
   url(r'^distribution/delete/(?P<pk>.*)/$',views.FileDeleteView.as_view(), name='dist_delete'),
+  url(r'^distribution/download/(?P<pk>.*)/$',views.DownloadView.as_view(), name='dist_download'),
 
   # 分類(カテゴリ)
   url(r'^category/$', views.CategoryIndexView.as_view(), name='category_index'),
--- submission_form/views/__init__.py
+++ submission_form/views/__init__.py
@@ -3,6 +3,7 @@ from submission_form.views.task import *
 from submission_form.views.upload import *
 from submission_form.views.StudentOrTeacherGetter import *
 from submission_form.views.DistributionController import *
+from submission_form.views.download import *
 
 from submission_form.views.service_start import *
 from submission_form.views.user_create import *
