b4379e76d418777607a6025af5debc7ce50366ee
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-02-05 14:10:44 +0000

    Add 詳細な名前を設定

--- submission_form/models/task.py
+++ submission_form/models/task.py
@@ -1,3 +1,4 @@
+from django.utils.translation import ugettext_lazy as _
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
@@ -6,13 +7,13 @@ from django.urls import reverse
 from submission_form.models import Organization, Classification
 
 class Task(models.Model):
-  organization_id = models.ForeignKey(Organization)
-  user_id = models.ForeignKey(settings.AUTH_USER_MODEL)
+  organization_id = models.ForeignKey(Organization, verbose_name = _('所属ID'))
+  user_id = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name = _('課題提出者'))
   classification_id = models.ForeignKey(Classification, verbose_name = '科目')
-  name = models.CharField(max_length = 64, verbose_name = '提出ファイル名')
-  text = models.TextField(verbose_name = '説明')
-  deadline = models.DateTimeField(verbose_name = '提出期限', help_text = 'xxxx-xx-xx の形式で入力してください')
-  published_date = models.DateTimeField(default = timezone.now, editable = False)
+  name = models.CharField(_('課題ファイル名'), max_length = 64)
+  text = models.TextField(_('課題説明'))
+  deadline = models.DateTimeField(_('提出期限'), help_text = 'xxxx-xx-xx の形式で入力してください')
+  published_date = models.DateTimeField(_('公開日'), default = timezone.now, editable = False)
 
   def get_absolute_url(self, *args, **kwargs):
     return reverse('submission_form:task_detail', kwargs = {'pk':self.pk})
