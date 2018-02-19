0afc8c0dbf5915ee66e40f6f762afb2030b816b4
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-02-19 09:11:07 +0000

    Update view name

--- submission_form/urls.py
+++ submission_form/urls.py
@@ -19,11 +19,11 @@ urlpatterns = [
   url(r'^task/(?P<pk>[0-9]+)/delete/$', views.TaskDeleteView.as_view(), name = 'task_delete'),
 
   # 配布物
-  url(r'^distribution/$', views.FileIndexView.as_view(), name='dist_index'),
-  url(r'^distribution/(?P<category_pk>[0-9]+)/$', views.FileCategoryView.as_view(), name='dist_category'),
-  url(r'^distribution/create/$', views.FileCreateView.as_view(), name='dist_create'),
-  url(r'^distribution/update/(?P<pk>.*)/$',views.FileUpdateView.as_view(), name='dist_update'),
-  url(r'^distribution/delete/(?P<pk>.*)/$',views.FileDeleteView.as_view(), name='dist_delete'),
+  url(r'^distribution/$', views.DistListView.as_view(), name='dist_index'),
+  url(r'^distribution/(?P<category_pk>[0-9]+)/$', views.DistClassListView.as_view(), name='dist_category'),
+  url(r'^distribution/create/$', views.DistUploadView.as_view(), name='dist_create'),
+  url(r'^distribution/update/(?P<pk>.*)/$',views.DistUpdateView.as_view(), name='dist_update'),
+  url(r'^distribution/delete/(?P<pk>.*)/$',views.DistDeleteView.as_view(), name='dist_delete'),
   url(r'^distribution/download/(?P<pk>.*)/$',views.DownloadDistView.as_view(), name='dist_download'),
 
   # 分類(カテゴリ)
