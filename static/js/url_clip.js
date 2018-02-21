ac92d4b6958c542b9935f594753edb75aac418f1
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-02-21 05:41:57 +0000

    Fix js function

--- static/js/url_clip.js
+++ static/js/url_clip.js
@@ -1,14 +1,11 @@
-<script>
-    const clipTeacher = function(){
-        const url = document.getElementById('teacher_link');
-        url.select();
-        document.execCommand('copy');
-    }
+function clipTeacher(){
+    const url = document.getElementById('teacher_link');
+    url.select();
+    document.execCommand('copy');
+}
 
-    const clipStudent = function(){
-        const url = document.getElementById('student_link');
-        url.select();
-        document.execCommand('copy');
-    }
-</script>
-
+function clipStudent(){
+    const url = document.getElementById('student_link');
+    url.select();
+    document.execCommand('copy');
+}
